8980066baf421391c87d9db1fa5a5254766122b8
Kagheni <Kaghenimbale@gmail.com>
2023-05-26 11:57:01 +0200

    Initialize my web page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,44 +1,9 @@
-import Image from 'next/image'
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30 text-gray-300">
-          Welcome to Promovize
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t lg:static lg:h-auto lg:w-auto">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0 text-white"
-            href="https://barackm.me"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              src="/assets/images/logo_yellow.png"
-              alt="Promovize Logo"
-              width={100}
-              height={100}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative "
-          src="/assets/images/logo_text.png"
-          alt="Next.js Logo"
-          width={400}
-          height={400}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 flex text-center lg:text-center">
-          <h3 className='text-3xl text-gray-300'>Coming soon...</h3>
-      </div>
+      PROMOVIZE
     </main>
-  )
+  );
 }
