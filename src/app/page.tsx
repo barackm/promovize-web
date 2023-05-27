a860d3d00b2d0fb5a2fc96040b94b7c34570b0cd
Kagheni <Kaghenimbale@gmail.com>
2023-05-27 10:58:50 +0200

    Add text-color to the header and woork on the footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from 'next/image';
 import Navbar from '../components/navbar/navbar';
 import Header from '@/components/Header/Header';
+import Footer from '@/components/Footer/Footer';
 
 export default function Home() {
   return (
     <main>
       <Navbar />
       <Header />
+      <Footer />
     </main>
   );
 }
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import Image from 'next/image';
+import logo from '../../../public/logo.png';
+import styles from './footer.module.css';
+import { FiTwitter } from 'react-icons/fi';
+import { FiLinkedin } from 'react-icons/fi';
+import { FiInstagram } from 'react-icons/fi';
+
+const Footer = () => {
+  return (
+    <div className={styles.footer}>
+      <div className={styles.footer_container}>
+        <div className="logo">
+          <Image width="50" priority src={logo} alt="logo" />
+          <p className="name">PROMOVIZE</p>
+        </div>
+        <div className={styles.footer_description}>
+          <ul className={styles.social_media}>
+            <li>
+              <a href="#">
+                <FiTwitter className={styles.social_icon} />
+              </a>
+            </li>
+
+            <li>
+              <a href="#">
+                <FiLinkedin className={styles.social_icon} />
+              </a>
+            </li>
+
+            <li>
+              <a href="#">
+                <FiInstagram className={styles.social_icon} />
+              </a>
+            </li>
+          </ul>
+
+          <form className={styles.form}>
+            <label>
+              <input
+                className={styles.email_input}
+                type="email"
+                required
+                placeholder="Email Address"
+              />
+            </label>
+            <button type="submit" className={styles.submit_btn}>
+              Sign Up
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
