c750254601831c7563c4d88de2d61570ebfa36ab
Kagheni <Kaghenimbale@gmail.com>
2023-05-27 08:21:38 +0200

    Add transition to the navigation bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image';
 import Navbar from '../components/navbar/navbar';
+import Header from '@/components/Header/Header';
 
 export default function Home() {
   return (
     <main>
       <Navbar />
+      <Header />
     </main>
   );
 }
--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -2,6 +2,7 @@
 
 import React, { useEffect, useState } from 'react';
 import logo from '../../../public/logo.png';
+import whitelogo from '../../../public/logo_white_yellow.png';
 import Image from 'next/image';
 import styles from './navbar.module.css';
 import Link from 'next/link';
@@ -9,11 +10,28 @@ import Link from 'next/link';
 const Navbar = () => {
   const NavlinkArr = ['Home', 'About', 'Services', 'Contacts'];
 
+  const [scroll, setScroll] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 80) {
+        setScroll(true);
+      } else {
+        setScroll(false);
+      }
+    });
+  }, []);
   return (
-    <div className={styles.nav_container}>
+    <div className={scroll ? styles.nav_active : styles.nav_container}>
       <nav className={styles.navbar}>
         <div className={styles.logo}>
-          <Image className="logo" width="50" priority src={logo} alt="logo" />
+          <Image
+            className="logo"
+            width="50"
+            priority
+            src={scroll ? whitelogo : logo}
+            alt="logo"
+          />
           <p className={styles.name}>PROMOVIZE</p>
         </div>
 
